bf42406be36750ac2fadfcc1bd7fb847afc6fbe3
Steven Wheeler <steven@stevenwheeler.co.uk>
2025-02-27 23:50:38 +0000

    upped version and added jellyfin server check

--- components/configuration.py
+++ components/configuration.py
@@ -1,13 +1,13 @@
 import logging
 import os
 import sys
+import requests
 from dotenv import load_dotenv
-from flask import app
 
 # Set the version
-VERSION=2.4
+VERSION = 3.0
 
-# Load environment variables from the .env file if it exists, if it doesnt then exit
+# Load environment variables from the .env file if it exists, if it doesn't then exit
 load_dotenv()
 
 # List of required environment variables
@@ -39,14 +39,31 @@ log_level = getattr(logging, LOGGING_LEVEL, logging.INFO)
 logging.basicConfig(
     level=log_level,
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
-     handlers=[
+    handlers=[
         logging.StreamHandler(sys.stdout),  # Logs to console
         logging.FileHandler('app.log')  # Logs to file 'app.log'
     ]
 )
 
 # Login settings
-LOGIN_ENABLED = os.environ.get('LOGIN_ENABLED', "true")
-if LOGIN_ENABLED.upper() == "TRUE":
+LOGIN_ENABLED = os.environ.get('LOGIN_ENABLED', "true").lower() in ['true', '1', 't', 'y', 'yes']
+if LOGIN_ENABLED:
     JELLYFIN_SERVER_ADDRESS = os.environ.get('JELLYFIN_SERVER_ADDRESS', "http://localhost:8096")
-    JELLYFIN_AUTH_ENDPOINT = '/Users/authenticate/byname'
+    JELLYFIN_AUTH_ENDPOINT = '/Users/AuthenticateByName'
+    logging.info(f"Login system is enabled. Logins are being authenticated against {JELLYFIN_SERVER_ADDRESS}.")
+
+    # Check if the Jellyfin server is reachable
+    try:
+        response = requests.get(JELLYFIN_SERVER_ADDRESS)
+        if response.status_code == 200:
+            logging.info(f"Successfully connected to Jellyfin server at {JELLYFIN_SERVER_ADDRESS}.")
+            pass
+        else:
+            logging.error(f"Failed to reach Jellyfin server at {JELLYFIN_SERVER_ADDRESS}. Status code: {response.status_code}. Logins will not work, thus the application will exit.")
+            sys.exit(1)
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error connecting to Jellyfin server at {JELLYFIN_SERVER_ADDRESS}: {e}")
+        sys.exit(1)
+
+else:
+    logging.info(f"Login system is DISABLED. This is NOT recommended unless you have alternative security measures in place to prevent anyone from uploading to your server.")
